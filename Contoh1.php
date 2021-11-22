0d47c7718183891f6e3b07f528205ab2407bbf34
AsipaBurhannudin <asipaburhannurdin@gmail.com>
2021-11-22 12:55:57 +0700

    Push file Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Hallo Perkenalkan</h1>";
+		echo"Nama saya Asipa Burhannudin
+			saya tinggal di Kota Bogor
+			Hobi saya membaca, olahraga dan sebagainya";
+	}
+}
